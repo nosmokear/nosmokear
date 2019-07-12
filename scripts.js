2296777cea99b3f2fbdce7cfe9e9d84cc7a81767
nosmokear <52730079+nosmokear@users.noreply.github.com>
2019-07-11 23:51:45 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -10,6 +10,41 @@ window.onload = function() {
    segundos = 0;
    cigarros = 0;
    mediaCigarro = 0;
+   if(document.cookie == ''){
+     $("#myModal").modal('show')
+     document.cookie = 'primeira=False;'
+   }
+
+
+   var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo'],
+        datasets: [{
+            label: 'Quantidade de cigarros fumados na última semana',
+            backgroundColor: 'rgba(50, 200, 200, 0.5)',
+            borderColor: 'rgb(80, 230, 230)',
+            data: [13, 8, 10, 9, 15, 18, 5]
+        }, {
+          label: 'Média de cigarros',
+          backgroundColor: 'rgba(30, 99, 132,0.5)',
+          borderColor: 'rgb(50,36,50)',
+          data: [10, 10, 7, 17, 20, 6]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+}
+
+function fechaModal(){
+  console.log($("#dataInicio").value)
+  $("#myModal").modal('hide')
 }
 
 var iniciou = false;
@@ -54,7 +89,7 @@ function atualizaTM(){
 }
 
 function atualizaDinheiro(){
-  dinheiro.innerHTML = "R$ " + (cigarros*9.45).toFixed(2);
+  dinheiro.innerHTML = "R$ " + (cigarros*0.47).toFixed(2);
 }
 
 function atualizaTotal(hh, mm, ss){
@@ -69,22 +104,22 @@ function atualizaTotal(hh, mm, ss){
   //
   tempoSegundos = (horas * 3600) + (minutos * 60) + (segundos);
   //
-  if(tempoSegundos >= 3){
+  if(tempoSegundos >= 3 && tempoSegundos < 6){
     pulmao.classList.remove("p1");
     pulmao.classList.add("p2");
-  } else if(tempoSegundos >= 6){
+  } else if(tempoSegundos >= 6 && tempoSegundos < 9){
     pulmao.classList.remove("p2");
     pulmao.classList.add("p3");
-  } else if(tempoSegundos >= 9){
+  } else if(tempoSegundos >= 9 && tempoSegundos < 12){
     pulmao.classList.remove("p3");
     pulmao.classList.add("p4");
-  } else if(tempoSegundos >= 12){
+  } else if(tempoSegundos >= 12 && tempoSegundos < 15){
     pulmao.classList.remove("p4");
     pulmao.classList.add("p5");
-  } else if(tempoSegundos >= 15){
+  } else if(tempoSegundos >= 15 && tempoSegundos < 18){
     pulmao.classList.remove("p5");
     pulmao.classList.add("p6");
-  } else if(tempoSegundos >= 18){
+  } else if(tempoSegundos >= 18 && tempoSegundos < 20){
     pulmao.classList.remove("p6");
     pulmao.classList.add("p7");
   } else if(tempoSegundos >= 21){
